f6259ceab4cfc1835f8fc180a21d054bf6dd5f81
ktsn <ktsn55@gmail.com>
2016-04-10 01:18:36 +0900

    Add History class

--- src/history.ts
+++ src/history.ts
@@ -1,5 +1,34 @@
 import { IOperation } from './interfaces/operation';
+import { AddOperation } from './operations/add-operation';
+import { RemoveOperation } from './operations/remove-operation';
 
-export class History<T> {
-  private commands: IOperation<T>[];
+export class History {
+  private operations: IOperation<string>[] = [];
+
+  constructor(public text: string = '') {
+
+  }
+
+  public addText(index: number, chunk: string) {
+    const op = new AddOperation(index, chunk);
+    this.applyOperation(op);
+  }
+
+  public removeText(index: number, length: number) {
+    const op = new RemoveOperation(index, length);
+    this.applyOperation(op);
+  }
+
+  public undo(index: number = this.operations.length - 1) {
+    const inverse = this.operations[index].inverse();
+    const transformed = this.operations.slice(index + 1).reduce((memo, opposed) => {
+      return memo.transform(opposed);
+    }, inverse);
+    this.applyOperation(transformed);
+  }
+
+  private applyOperation(op: IOperation<string>) {
+    this.text = op.apply(this.text);
+    this.operations.push(op);
+  }
 }
